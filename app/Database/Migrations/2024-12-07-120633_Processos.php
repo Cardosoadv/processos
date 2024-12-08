9f8cdd88f22b28056c4e94a333131ffb42409d82
Cardoso <fabianocardoso.adv@gmail.com>
2024-12-08 09:35:01 -0300

    Criando migration para processos e intimações

---
+++ app/Database/Migrations/2024-12-07-120633_Processos.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Processos extends Migration
+{
+    public function up(){
+
+
+        $this->forge->addField([
+            'id_processo'               => ['type' => 'int', 'constraint' => 11],
+            'siglaTribunal'             => ['type' => 'varchar', 'constraint' => 10, 'null' => true],
+            'nomeOrgao'                 => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'numero_processo'           => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'link'                      => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'tipoDocumento'             => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'codigoClasse'              => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'ativo'                     => ['type' => 'tinyint','default' => 1],
+            'status'                    => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'numeroprocessocommascara'  => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'risco'                     => ['type' => 'ENUM', 'constraint'=>['Provável', 'Possível', 'Remoto'], 'default' => 'Possível'],    
+            'created_at'                => ['type' => 'datetime', 'null' => true],
+            'updated_at'               => ['type' => 'datetime', 'null' => true],
+            'deleted_at'                => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id_processo');
+        $this->forge->addUniqueKey('numero_processo', 'numero_processo');
+        $this->forge->createTable('processos');
+
+
+        $this->forge->addField([
+            'id_intimacao'              => ['type' => 'int', 'constraint' => 11],
+            'data_disponibilizacao'     => ['type' => 'date', 'null' => true],
+            'tipoComunicacao'           => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'texto'                     => ['type' => 'longtext', 'null' => true],
+            'numero_processo'           => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'meio'                      => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'link'                      => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'numeroComunicacao'         => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'hash'                      => ['type' => 'varchar', 'constraint'=>50, 'null' => true],
+            'motivo_cancelamento'       => ['type' => 'varchar', 'constraint'=>250, 'null' => true],
+            'data_cancelamento'         => ['type' => 'date', 'null' => true],
+            'datadisponibilizacao'      => ['type' => 'date', 'null' => true],
+            'dataenvio'                 => ['type' => 'date', 'null' => true],
+            'meiocompleto'              => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'created_at'                => ['type' => 'datetime', 'null' => true],
+            'updated_at'                => ['type' => 'datetime', 'null' => true],
+            'deleted_at'                => ['type' => 'datetime', 'null' => true],
+        ]);
+        $this->forge->addPrimaryKey('id_intimacao');
+        $this->forge->addForeignKey('numero_processo', 'processos', 'numero_processo', '', '', 'FK_intimacoes_numero_processo');
+        $this->forge->createTable('intimacoes');
+
+        $this->forge->addField([      
+            'id_pk'              => ['type' => 'int', 'constraint' => 11, 'auto_increment' => true],
+            'nome'               => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'polo'               => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'comunicacao_id'     => ['type' => 'int', 'constraint' => 11],
+
+        ]);
+        $this->forge->addPrimaryKey('id_pk');
+        $this->forge->addForeignKey('comunicacao_id', 'intimacoes', 'id_intimacao', '', '', 'FK_intimacoes_destinatario');
+        $this->forge->createTable('intimacoes_destinatario');
+
+
+        $this->forge->addField([  
+            'id_pk'              => ['type' => 'int', 'constraint' => 11, 'auto_increment' => true],    
+            'id'                => ['type' => 'int', 'constraint'=>11],
+            'comunicacao_id'    => ['type' => 'int', 'constraint'=>11, 'null' => true],
+            'advogado_id'       => ['type' => 'int', 'constraint'=>11, 'null' => true],    
+            'advogado_nome'     => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'advogado_oab'      => ['type' => 'varchar', 'constraint'=>150, 'null' => true],
+            'advogado_oab_uf'   => ['type' => 'varchar', 'constraint'=>10, 'null' => true],
+            'created_at'        => ['type' => 'datetime', 'null' => true],
+            'updated_at'        => ['type' => 'datetime', 'null' => true],
+
+        ]);
+        $this->forge->addPrimaryKey('id_pk');
+        $this->forge->addForeignKey('comunicacao_id', 'intimacoes', 'id_intimacao', '', '', 'FK_intimacoes_advogados');
+        $this->forge->createTable('intimacoes_advogados');
+    }
+
+    public function down()
+    {
+        $this->forge->dropForeignKey('numero_processo', 'FK_intimacoes_numero_processo');
+        $this->forge->dropForeignKey('intimacoes_destinatario', 'FK_intimacoes_destinatario');
+        $this->forge->dropForeignKey('intimacoes_advogados', 'FK_intimacoes_advogados');
+        $this->forge->dropTable('processos');
+        $this->forge->dropTable('intimacoes_destinatario');
+        $this->forge->dropTable('intimacoes_advogados');
+        $this->forge->dropTable('intimacoes');
+    }
+}
