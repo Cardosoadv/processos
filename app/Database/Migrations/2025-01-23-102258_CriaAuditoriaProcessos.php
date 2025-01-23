8f9aed02acbd23da307228489d5de0c1acd9c4c4
Cardoso <fabianocardoso.adv@gmail.com>
2025-01-23 07:26:20 -0300

    cria migration auditoria de processo

---
+++ app/Database/Migrations/2025-01-23-102258_CriaAuditoriaProcessos.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProcessoAuditTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_audit' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'processo_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'user_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'action_type' => [
+                'type'       => 'ENUM',
+                'constraint' => ['CREATE', 'UPDATE', 'DELETE'],
+            ],
+            'changes' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'ip_address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 45,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id_audit', true);
+        $this->forge->addForeignKey('processo_id', 'processos', 'id_processo', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'SET NULL', 'SET NULL');
+        $this->forge->createTable('auditoria_processo');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('processo_audits');
+    }
+}
