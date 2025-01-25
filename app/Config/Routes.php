6c8fe326afcc1664bb0135f70cc316a1c05f24cc
Cardoso <fabianocardoso.adv@gmail.com>
2025-01-25 15:33:58 -0300

    adiciona rota para salvar objetos de processo

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,4 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+$routes->get('/processoobjeto/salvar', 'ProcessoObjeto::salvar');
+
 service('auth')->routes($routes);
