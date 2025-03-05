c7630768a6a821443f01af69913b4565e4cf45b1
Cardoso <fabianocardoso.adv@gmail.com>
2025-03-05 11:14:25 -0300

    adiciona cálculo de saldo entre receitas e despesas no controlador financeiro

--- app/Controllers/Financeiro/Index.php
+++ app/Controllers/Financeiro/Index.php
@@ -11,6 +11,27 @@ class Index extends BaseController
         $data = [
             'titulo' => 'Financeiro',
         ];
+        $conta_id = 1;
+
+        // Instanciar os models
+        $despesasModel = model('Financeiro/FinanceiroPagtoDespesasModel');
+        $receitasModel = model('Financeiro/FinanceiroPagtoReceitasModel');
+
+        // Calcular o total de despesas para a conta específica
+        $totalDespesas = $despesasModel ->where('conta_id', $conta_id)
+                                        ->selectSum('valor')
+                                        ->get()
+                                        ->getRowArray()['valor'];
+
+        // Calcular o total de receitas para a conta específica
+        $totalReceitas = $receitasModel ->where('conta_id', $conta_id)
+                                        ->selectSum('valor')
+                                        ->get()
+                                        ->getRowArray()['valor'];
+
+        // Calcular o saldo (Receitas - Despesas)
+        $data['saldo'] = $totalReceitas - $totalDespesas;
+
         return view('financeiro/index', $data);
     }
 }
