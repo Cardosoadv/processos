d7ed4f21edbbafc588f96c3a04097fbd2b87b4d1
Cardoso <fabianocardoso.adv@gmail.com>
2025-03-18 16:49:32 -0300

    adiciona dropdowns de mensagens, notificações e menu de usuário na navegação

--- app/Views/template/componentes/nav/userMenuDropdown.php
+++ app/Views/template/componentes/nav/userMenuDropdown.php
@@ -1,17 +1,29 @@
 <li class="nav-item dropdown user-menu"><a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-    <img src="<?= base_url('public/dist/assets/img/user1-128x128.jpg') ?>" class="user-image rounded-circle shadow img-size-50" alt="User Image">
+    <img src="<?= base_url('public/dist/assets/img/user1-128x128.jpg') ?>" class="user-image rounded-circle shadow" alt="User Image">
     <span class="d-none d-md-inline"><?= auth()->user()->username ?></span></a>
-    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
-        <!--begin::User Image-->
+    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"><!--begin::User Image-->
         <li class="user-header text-bg-primary">
-            <a data-bs-toggle="modal" data-bs-target="#modal-alterar-foto-perfil">
-
-                <img src="<?= base_url('public/dist/assets/img/user1-128x128.jpg')  ?>" class="rounded-circle shadow img-size-50" alt="User Image"></a>
+        <a data-bs-toggle="modal" data-bs-target="#modal-alterar-foto-perfil">
+            <img src="<?= base_url('public/dist/assets/img/user1-128x128.jpg') ?>" class="rounded-circle shadow" alt="User Image">    
+        </a>
             <p>
                 <?= auth()->user()->username ?>
             </p>
-        </li><!--end::User Image-->
-        <!--begin::Menu Footer-->
-        <li class="user-footer"><a href="#" class="btn btn-default btn-flat">Profile</a><a href="<?= base_url('logout') ?>" class="btn btn-default btn-flat float-end">Sign out</a></li><!--end::Menu Footer-->
+        </li><!--end::User Image--><!--begin::Menu Body-->
+        <li class="user-body"><!--begin::Row-->
+            <div class="row">
+                <div class="col-4 text-center"><a href="#">Followers</a></div>
+                <div class="col-4 text-center"><a href="#">Sales</a></div>
+                <div class="col-4 text-center"><a href="#">Friends</a></div>
+            </div><!--end::Row-->
+        </li><!--end::Menu Body--><!--begin::Menu Footer-->
+        <li class="user-footer">
+            <a href="#" class="btn btn-default btn-flat">
+                Profile
+            </a>
+            <a href="<?= base_url('logout') ?>" class="btn btn-default btn-flat float-end">
+                Sign out
+            </a>
+        </li><!--end::Menu Footer-->
     </ul>
 </li>
