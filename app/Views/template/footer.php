b9040e6edf9ba221ebbcd6749ed63f67ecf797e6
Cardoso <fabianocardoso.adv@gmail.com>
2025-03-21 18:30:46 -0300

    atualiza rodapé para incluir novos créditos e ano de copyright, melhora a formatação do código

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,25 +1,26 @@
 <div id="toast-container" class="position-fixed top-0 end-0 p-5" style="z-index: 11"></div>
 <!--begin::Footer-->
 <footer class="app-footer"><!--begin::To the end-->
-            <div class="float-end d-none d-sm-inline">Anything you want</div><!--end::To the end--><!--begin::Copyright--><strong>
-                Copyright &copy; 2014-2023&nbsp;
-                <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>.
-            </strong>
-            All rights reserved.
-            <!--end::Copyright-->
-        </footer><!--end::Footer-->
-    </div><!--end::App Wrapper--><!--begin::Script--><!--begin::Third Party Plugin(OverlayScrollbars)-->
-    <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-H2VM7BKda+v2Z4+DRy69uknwxjyDRhszjXFhsL4gD3w=" crossorigin="anonymous"></script><!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script><!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    <script src="<?= base_url('public/dist/js/adminlte.js')?>"></script><!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js" integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8=" crossorigin="anonymous"></script><!-- ChartJS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" crossorigin></script>
+    <div class="float-end d-none d-sm-inline">Powered by <a href="https://adminlte.io" class="text-decoration-none">AdminLTE.io</a>
+    </div><!--end::To the end--><!--begin::Copyright--><strong>
+        Copyright &copy; 2025&nbsp;
+        <a href="https://www.fabianocardoso.com.br" class="text-decoration-none">Fabiano Cardoso</a>.
+    </strong>
+    All rights reserved.
+    <!--end::Copyright-->
+</footer><!--end::Footer-->
+</div><!--end::App Wrapper--><!--begin::Script--><!--begin::Third Party Plugin(OverlayScrollbars)-->
+<script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-H2VM7BKda+v2Z4+DRy69uknwxjyDRhszjXFhsL4gD3w=" crossorigin="anonymous"></script><!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
+<script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script><!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
+<script src="<?= base_url('public/dist/js/adminlte.js') ?>"></script><!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
+<script src="https://cdn.jsdelivr.net/npm/apexcharts@3.37.1/dist/apexcharts.min.js" integrity="sha256-+vh8GkaU7C9/wbSLIcwq82tQ2wTf44aOHA8HlBMwRI8=" crossorigin="anonymous"></script><!-- ChartJS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" crossorigin></script>
 
-    <script>
-        function mostrarMensagem(mensagem, tipo) {
-            toastr[tipo](mensagem);
-}
-    </script>
+<script>
+    function mostrarMensagem(mensagem, tipo) {
+        toastr[tipo](mensagem);
+    }
+</script>
